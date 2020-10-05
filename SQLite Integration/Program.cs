4f4182182a13ccc9b5f95623fe392c5249610dc7
jam1nb3n <unconfigured@null.spigotmc.org>
2020-10-05 11:59:57 +0100

    Edit

--- SQLite Integration/Program.cs
+++ SQLite Integration/Program.cs
@@ -12,28 +12,8 @@ namespace SQLite_Integration
     {
         static void Main(string[] args)
         {
-            DebugCreateUser();
+            
         }
-        static void DebugCreateUser()
-        {
-            UserModel newUser = new UserModel();
-            newUser.Username = "FUCKOOFF";
-            newUser.Password = "yteee1";
-            newUser.DateJoined = (DateTime.Now).ToString();
-
-            SQLiteDataAccess.LoadUserData(newUser.Username, out int status2);
-
-            if (status2 == 1)
-            {
-
-                SQLiteDataAccess.SaveNewUser(newUser, out int status);
-                Debug.WriteLine(status);
-            }
-            else
-            {
-                Console.WriteLine("user already exists");
-                Console.ReadLine();
-            }
-        }
+        
     }
 }
